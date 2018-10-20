fa4497bb986f0e29e053c190ea39d5f85b205285
renaism <zafitract@gmail.com>
2018-10-20 18:40:58 +0700

    remove django project

--- bahan/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BahanConfig(AppConfig):
-    name = 'bahan'
--- bahan/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Bahan(models.Model):
-    nama_bahan = models.CharField(max_length=200)
-    satuan = models.CharField(max_length=50)
-    stok = models.DecimalField()
