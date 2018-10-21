73574453fedd74c56cf78a4b953e78a1fd3d32c5
renaism <zafitract@gmail.com>
2018-10-21 12:35:26 +0700

    add basic landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Manajemen Bakso Akup</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/materialize.min.css') }}" rel="stylesheet">
+    <style>
+        .container {
+            margin-top: 200px;
+        }
+    </style>
+</head>
+<body class="red darken-4">
+    <div class="container center-align">
+        <div class="row">
+            <div class="col s12">
+                <img src="{{ asset('mie_akup.png') }}" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m3 offset-m2">
+                <div class="card grey darken-4">
+                    <div class="card-content">
+                            <a href="#" class="card-title white-text">Kasir</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m3 offset-m2">
+                <div class="card grey darken-4">
+                    <div class="card-content">
+                        <a href="#" class="card-title white-text">Manajer</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
